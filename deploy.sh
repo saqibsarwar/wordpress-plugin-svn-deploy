9ac0bcb1e77a6025f16604a44882e388e961714b
Saqib Sarwar <saqib@inspirythemes.com>
2020-02-29 10:46:09 +0500

    Updated default username

--- deploy.sh
+++ deploy.sh
@@ -44,7 +44,7 @@ echo
 CURRENTDIR=$(pwd)
 default_svnpath="/tmp/$PLUGINSLUG"
 default_svnurl="https://plugins.svn.wordpress.org/$PLUGINSLUG"
-default_svnuser="GaryJ"
+default_svnuser="saqibsarwar"
 default_plugindir="$CURRENTDIR/$PLUGINSLUG"
 default_mainfile="$PLUGINSLUG.php"
 default_assetsdir=".wordpress-org"
